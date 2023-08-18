a9b0cb8da3b09e8a3de3fcebc7f3a584b6062e88
Cleilton Rocha Barreto <123566534+Cleiltoncrb@users.noreply.github.com>
2023-08-17 23:57:27 -0300

    TypeScript atualização de casa

--- JAVA-SCRIPT/TYPESCRIPT/app/models/negociacao.ts
+++ JAVA-SCRIPT/TYPESCRIPT/app/models/negociacao.ts
@@ -1,22 +1,18 @@
 export class Negociacao {
 
     constructor(
-        private _data: Date,
-        private _quantidade: number,
-        private _valor: number
-    ){ }
+        private readonly _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number
+    ) { }
+
+
+    get volume(): number {
+        return this.quantidade * this.valor;
+    }
 
     get data(): Date {
-        return this._data;
+        const date = new Date(this._data.getTime());
+        return date;
     }
-    get quantidade(): number {
-        return this._quantidade;
-    }
-    get valor(): number {
-        return this._valor;
-    }
-    get volume(): number {
-        return this._quantidade * this._valor;
-    }
-
 }
