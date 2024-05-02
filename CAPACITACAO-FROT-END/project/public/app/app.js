fd26b0535c7256b69c274b7078b584aadae17d0f
Cleilton-CCABR <cleiltoncrb@fab.mil.br>
2024-05-02 16:59:38 -0300

    atualização

---
+++ CAPACITACAO-FROT-END/project/public/app/app.js
@@ -0,0 +1,19 @@
+import { log } from "./utils/promise-helpers.js";
+import './utils/array-helpers.js';
+import { notasService as service } from "./nota/service.js";
+import { debounceTime, takeUntil } from "./utils/operators.js";
+
+
+const action = debounceTime(500, 
+    takeUntil(3, () =>
+      service
+      .somaDeItens('2143')
+      .then(console.log)
+      .catch(console.log)
+    )
+);
+
+document
+.querySelector('#myButton')
+.onclick = action;
+
---
+++ CAPACITACAO-FROT-END/project/public/app/nota/service.js
@@ -0,0 +1,32 @@
+import { handleStatus } from "../utils/promise-helpers.js";
+import { partialize, pipe } from "../utils/operators.js";
+
+const API = 'http://localhost:3000/notas';
+
+const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo == code);
+const sumItemsValues = items => items.reduce((total, item) => total + item.valor, 0);
+
+export const notasService = {
+ listAll(){
+    return fetch(API)
+    .then(handleStatus)
+    .catch(error => {
+      console.log(error);
+      return Promise.reject('Não foi possível obter as notars fiscais!');
+    });
+  }, 
+
+  somaDeItens(code){
+    const filterItems = partialize(filterItemsByCode, code);
+
+    const sumItems = pipe(
+      getItemsFromNotas, 
+      filterItems, 
+      sumItemsValues
+      );
+
+    return this.listAll()
+    .then(sumItems);
+  }
+}
